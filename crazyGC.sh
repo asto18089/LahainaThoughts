2ba9e1f59ccfa32901f220f228b0b81294d7ea50
asto18089 <asto18089@126.com>
2022-08-29 15:54:25 +0100

    Add fstrim mechanism

--- crazyGC.sh
+++ crazyGC.sh
@@ -8,11 +8,19 @@ echo 131072 > /sys/fs/f2fs/dm-10/max_victim_search
 # Call gc_urgent and UFSHID in a loop, assuming the device mapper's number is 10.
 # UFSHID is a shorthand for UFS Host Initiated Defrag, which probably means the Host(LAHAINA) will ask the device(UFS) to defrag?
 # UFSHID is not supported on all UFS chips. Oneplus 9 Pro's SAMSUNG 3D-V5 F/W Ver 1903 support it, but F/W 1302 and F/W 090x do not.
-a=0
-while [ $a -lt 100 ]
+
+# The number of moved block will be displayed per iteration of the loop. 
+# When the number becomes steady (i.e. stable or increasing sigificantly slower), the GC is completed.
+while true
 do
-    sync
-    echo 1 > /sys/fs/f2fs/dm-10/gc_urgent
-    echo 1 > /sys/devices/platform/soc/1d84000.ufshc/ufshid/trigger
-    cat /sys/fs/f2fs/dm-10/moved_blocks_background
+    a=0
+    while [ $a -lt 100 ]
+    do
+        sync
+        echo 1 > /sys/fs/f2fs/dm-10/gc_urgent
+        echo 1 > /sys/devices/platform/soc/1d84000.ufshc/ufshid/trigger
+        cat /sys/fs/f2fs/dm-10/moved_blocks_background
+        ((a++))
+    done
+    fstrim /data
 done
